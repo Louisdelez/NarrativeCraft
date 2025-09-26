9b3a39c0a5c0d4bb8a2b5bb44957a20e84d21a52
LOUDO <lucaslelouer@gmail.com>
2025-09-26 22:08:34 +0200

    Unlink character from animation and remove it from camera angle if removed

--- common/src/main/java/fr/loudo/narrativecraft/screens/storyManager/character/CharactersScreen.java
+++ common/src/main/java/fr/loudo/narrativecraft/screens/storyManager/character/CharactersScreen.java
@@ -26,7 +26,10 @@ package fr.loudo.narrativecraft.screens.storyManager.character;
 import fr.loudo.narrativecraft.NarrativeCraftMod;
 import fr.loudo.narrativecraft.files.NarrativeCraftFile;
 import fr.loudo.narrativecraft.managers.CharacterManager;
+import fr.loudo.narrativecraft.narrative.chapter.Chapter;
 import fr.loudo.narrativecraft.narrative.chapter.scene.Scene;
+import fr.loudo.narrativecraft.narrative.chapter.scene.data.Animation;
+import fr.loudo.narrativecraft.narrative.chapter.scene.data.CameraAngle;
 import fr.loudo.narrativecraft.narrative.character.CharacterStory;
 import fr.loudo.narrativecraft.screens.characters.CharacterEntityTypeScreen;
 import fr.loudo.narrativecraft.screens.components.EditInfoScreen;
@@ -35,6 +38,7 @@ import fr.loudo.narrativecraft.screens.storyManager.StoryElementScreen;
 import fr.loudo.narrativecraft.screens.storyManager.scene.ScenesMenuScreen;
 import fr.loudo.narrativecraft.util.ImageFontConstants;
 import fr.loudo.narrativecraft.util.Translation;
+import fr.loudo.narrativecraft.util.Util;
 import java.util.List;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
@@ -104,6 +108,36 @@ public class CharactersScreen extends StoryElementScreen {
                                 } else {
                                     scene.removeNpc(character);
                                     NarrativeCraftFile.deleteCharacterFolder(character, scene);
+                                }
+                                try {
+                                    for (Chapter chapter : NarrativeCraftMod.getInstance()
+                                            .getChapterManager()
+                                            .getChapters()) {
+                                        for (Scene scene1 : chapter.getSortedSceneList()) {
+                                            for (Animation animation : scene1.getAnimations()) {
+                                                if (animation
+                                                        .getCharacter()
+                                                        .getName()
+                                                        .equalsIgnoreCase(character.getName())) {
+                                                    animation.setCharacter(null);
+                                                    NarrativeCraftFile.updateAnimationFile(animation);
+                                                }
+                                            }
+                                            for (CameraAngle cameraAngle : scene1.getCameraAngles()) {
+                                                cameraAngle
+                                                        .getCharacterStoryDataList()
+                                                        .removeIf(characterStoryData -> characterStoryData
+                                                                .getCharacterStory()
+                                                                .getName()
+                                                                .equalsIgnoreCase(character.getName()));
+                                            }
+                                            NarrativeCraftFile.updateCameraAngles(scene1);
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    Util.sendCrashMessage(minecraft.player, e);
+                                    minecraft.setScreen(null);
+                                    return;
                                 }
                                 minecraft.setScreen(new CharactersScreen(scene));
                             });
