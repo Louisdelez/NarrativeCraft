899370ca19facec525cd179e020952c0a6c52508
LOUDO <lucaslelouer@gmail.com>
2025-10-05 13:50:28 +0200

    Fixed forge production mixin crash

--- forge/src/main/java/fr/loudo/narrativecraft/NarrativeCraftForge.java
+++ forge/src/main/java/fr/loudo/narrativecraft/NarrativeCraftForge.java
@@ -23,9 +23,12 @@
 
 package fr.loudo.narrativecraft;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
-import fr.loudo.narrativecraft.mixin.accessor.RenderStateShardForgeAccessor;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.fml.common.Mod;
 
@@ -42,12 +45,17 @@ public class NarrativeCraftForge {
                 false,
                 true,
                 RenderType.CompositeState.builder()
-                        .setShaderState(RenderStateShardForgeAccessor.getPOSITION_COLOR_SHADER())
-                        .setTransparencyState(RenderStateShardForgeAccessor.getTRANSLUCENT_TRANSPARENCY())
-                        .setTextureState(RenderStateShardForgeAccessor.getNO_TEXTURE())
-                        .setDepthTestState(RenderStateShardForgeAccessor.getNO_DEPTH_TEST())
-                        .setCullState(RenderStateShardForgeAccessor.getNO_CULL())
-                        .setWriteMaskState(RenderStateShardForgeAccessor.getCOLOR_WRITE())
+                        .setShaderState(new RenderStateShard.ShaderStateShard(GameRenderer::getPositionColorShader))
+                        .setTransparencyState(new RenderStateShard.TransparencyStateShard("translucent_transparency", () -> {
+                            RenderSystem.enableBlend();
+                            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+                        }, () -> {
+                            RenderSystem.disableBlend();
+                            RenderSystem.defaultBlendFunc();
+                        }))
+                        .setTextureState(new RenderStateShard.EmptyTextureStateShard(() -> {}, () -> {}))
+                        .setDepthTestState(new RenderStateShard.DepthTestStateShard("always", 519))
+                        .setCullState(new RenderStateShard.CullStateShard(false))
                         .createCompositeState(false));
     }
 }
