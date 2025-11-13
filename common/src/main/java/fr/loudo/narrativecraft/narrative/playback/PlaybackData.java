9f68e72248ba6eb2b09c5194f20aa22e217db57f
LOUDO <lucaslelouer@gmail.com>
2025-11-13 15:14:12 +0100

    Fix

--- common/src/main/java/fr/loudo/narrativecraft/narrative/playback/PlaybackData.java
+++ common/src/main/java/fr/loudo/narrativecraft/narrative/playback/PlaybackData.java
@@ -102,8 +102,8 @@ public class PlaybackData {
             if (entity instanceof FakePlayer fakePlayer) {
                 Util.removeFakePlayerUUID(fakePlayer);
             }
+            entity = null;
         });
-        entity = null;
     }
 
     public void spawnEntity(Location location) {
