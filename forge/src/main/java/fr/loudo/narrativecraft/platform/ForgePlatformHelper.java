e60799ec53f890b73c2fffc7d154739899337775
LOUDO <lucaslelouer@gmail.com>
2025-11-13 13:37:40 +0100

    grrrrrrr

--- forge/src/main/java/fr/loudo/narrativecraft/platform/ForgePlatformHelper.java
+++ forge/src/main/java/fr/loudo/narrativecraft/platform/ForgePlatformHelper.java
@@ -41,6 +41,6 @@ public class ForgePlatformHelper implements IPlatformHelper {
 
     @Override
     public boolean isDevelopmentEnvironment() {
-        return !FMLLoader.getCurrent().isProduction();
+        return !FMLLoader.isProduction();
     }
 }
