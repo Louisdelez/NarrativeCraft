1371804eb855799a0fcf5cb243e3d60d7d7d6025
LOUDO <lucaslelouer@gmail.com>
2025-11-22 17:36:43 +0100

    Updated version

--- common/src/main/java/fr/loudo/narrativecraft/NarrativeCraftMod.java
+++ common/src/main/java/fr/loudo/narrativecraft/NarrativeCraftMod.java
@@ -40,7 +40,7 @@ public class NarrativeCraftMod {
     public static final String MOD_ID = "narrativecraft";
     public static final String MOD_NAME = "NarrativeCraft";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
-    public static final String VERSION = "1.0.0";
+    public static final String MAJOR_VERSION = "1.0.0";
 
     public static boolean firstTime = false;
     public static MinecraftServer server;
